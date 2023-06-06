671e915c78ad6a7169a2a1c90d1c57884d68d801
Serking de Orayom <kingdeorayom@gmail.com>
2023-06-06 19:23:04 +0800

    update: search keyword

--- routes/search.js
+++ routes/search.js
@@ -3,15 +3,15 @@ const router = express.Router()
 
 const VehicleModel = require('../models/vehicle')
 
-router.get('/:key', async (request, response) => {
+router.get('/:keyword', async (request, response) => {
 
     let data = await VehicleModel.find({
 
         "$or": [
-            { name: { $regex: request.params.key, $options: 'i' } },
-            { brand: { $regex: request.params.key, $options: 'i' } },
-            { model: { $regex: request.params.key, $options: 'i' } },
-            { type: { $regex: request.params.key, $options: 'i' } },
+            { name: { $regex: request.params.keyword, $options: 'i' } },
+            { brand: { $regex: request.params.keyword, $options: 'i' } },
+            { model: { $regex: request.params.keyword, $options: 'i' } },
+            { type: { $regex: request.params.keyword, $options: 'i' } },
         ]
     })
 
