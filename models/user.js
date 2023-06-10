4e888795cc0a92cc7898767cac3759055b2d0ed7
Serking de Orayom <kingdeorayom@gmail.com>
2023-06-10 10:06:21 +0800

    update: login and register

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
-    username: {
+    email: {
         type: String,
         required: true,
         unique: true
@@ -11,5 +13,62 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// static signup method
+
+userSchema.statics.signup = async function (email, password) {
+
+    // validation
+
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password is not strong enough')
+    }
+
+    const emailExists = await this.findOne({ email })
+
+    if (emailExists) {
+        throw Error('Email is already in use.')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+
+}
+
+// static login method
+
+userSchema.statics.login = async function (email, password) {
+
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Invalid login credentials')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Invalid login credentials')
+    }
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,47 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 
 const UserModel = require('../models/user')
 
+const createToken = (_id) => {
+    return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: '3d' })
+}
+
 router.post('/login', async (request, response) => {
-    response.json({ message: "Login user" })
+
+    const { email, password } = request.body
+
+    try {
+        const user = await UserModel.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        response.status(200).json({ email, token })
+
+    } catch (error) {
+        response.status(400).json({ error: error.message })
+    }
+
 })
 
 router.post('/register', async (request, response) => {
-    response.json({ message: "Register user" })
+
+    const { email, password } = request.body
+
+    try {
+        const user = await UserModel.signup(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        response.status(200).json({ email, token })
+
+    } catch (error) {
+        response.status(400).json({ error: error.message })
+    }
+
 })
 
 
